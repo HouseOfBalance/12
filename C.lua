4c4cdff1f25227dbc23bb9261ffc78f29135855d
HouseOfBalance <153303762+HouseOfBalance@users.noreply.github.com>
2024-12-25 23:26:15 +0700

    Create C.lua

---
+++ C.lua
@@ -0,0 +1,10 @@
+**💖 Thank you for purchasing MARU HUB 
+ขอบคุณสำหรับการซื้อสคริปต์ MARU HUB 💖 
+
+⊹‧₊˚ สคริปต์ซัพพอร์ตตัวรัน / Support [ Krampus Only ]** 
+
+```lua
+(getgenv()).key = "MARUTVWCM4UBSG9U1S868JKO3D";
+(getgenv()).id = "858142859777146950";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+```
