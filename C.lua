a702a3a0737862a3ecfa69e730aeda678741bd03
HouseOfBalance <153303762+HouseOfBalance@users.noreply.github.com>
2024-12-25 23:44:38 +0700

    Update C.lua

--- C.lua
+++ C.lua
@@ -3,3 +3,4 @@ repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "hXLnjtZmIIySVTwhuXNflsHsAqaMqeMq" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
 ```
+loadstring(game:HttpGet("https://isnahamzahpastebin.tech/sepuh/ghub11"))()
