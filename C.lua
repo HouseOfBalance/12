dd95be43c20bf59c92f6297ecacf75e5bd8633c0
HouseOfBalance <153303762+HouseOfBalance@users.noreply.github.com>
2024-12-25 23:36:36 +0700

    Update C.lua

--- C.lua
+++ C.lua
@@ -1,19 +1,6 @@
-Blox Fruit (kaitun):
+Blox Fruit (main):
 ```lua
--- Max level, godhuman
-getgenv().Configs = {
-    ["Team"] = "Pirates",
-    ["FPS Boost"] = {
-        ["Enable"] = false,
-        ["FPS Cap"] = 30,
-    },
-    ["Farm Boss Drops"] = {
-        ["Enable"] = false,
-        ["When x2 Exp Expired"] = false
-    },
-    ["Hop Player Near"] = true,
-    ["Soul Guitar"] = true,
-    ["Find Fruit"] = false, -- Will find 1m+ fruit to unlock swan door to access third sea
-    ["Cursed Dual Katana"] = true
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))()```
+getgenv().Team = "Marines"
+getgenv().Hide_Menu = false
+loadstring(game:HttpGet("https://apixerohub.x10.mx/main.lua"))()
+```
